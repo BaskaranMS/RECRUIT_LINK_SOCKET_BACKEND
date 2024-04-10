f0a04b88d500c5eb636cf6ca559cc838d74cd34a
BaskaranMS <official.ms.baskaran@gmail.com>
2024-04-10 09:45:33 +0530

    url update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const s3 = new AWS.S3({
 
 
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+mongoose.connect(process.env.MONGO_URL,  { useNewUrlParser: true, useUnifiedTopology: true } ).then(()=>{
     console.log('connected to mongodb!!');
 }).catch((err)=>{
     console.log('Error connecting to mongodb!!', err);
